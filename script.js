bd982d661675ff18c95ead0a53641c507ed6807d
xicobad <gmielikov@gmail.com>
2024-08-01 17:46:01 +0400

    slider done

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-   const menuButton = document.querySelector('.menu-img');
-   const menu = document.querySelector('.menu');
-   const closeButton = document.querySelector('.close-menu');
+   let menuButton = document.querySelector('.menu-img');
+   let menu = document.querySelector('.menu');
+   let closeButton = document.querySelector('.close-menu');
 
-   menuButton.addEventListener('click', function (e) {
-      e.preventDefault()
+   menuButton.addEventListener('click', function () {
       menu.classList.toggle('open');
    });
 
    closeButton.addEventListener('click', function () {
       menu.classList.remove('open');
+   });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+   let readMoreBtn = document.getElementById('readMoreBtn');
+   let hiddenSlides = document.querySelectorAll('.swiper-slide.hidden');
+   let readMoreImage = readMoreBtn.querySelector('img');
+
+   readMoreBtn.addEventListener('click', function () {
+      if (readMoreBtn.classList.contains('expanded')) {
+         hiddenSlides.forEach(slide => slide.classList.add('hidden'));
+         readMoreBtn.querySelector('span').textContent = 'Читать далее';
+         readMoreBtn.classList.remove('expanded');
+         readMoreImage.src = '/img/readmore/icon.svg';
+      } else {
+         hiddenSlides.forEach(slide => slide.classList.remove('hidden'));
+         readMoreBtn.querySelector('span').textContent = 'Спрятать';
+         readMoreBtn.classList.add('expanded');
+         readMoreImage.src = '/img/readmore/expand.svg';
+      }
    });
 });
--- swiper.js
+++ swiper.js
@@ -1,8 +1,10 @@
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 'auto',
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+if (window.innerWidth < 768 || window.innerWidth > 1119) {
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
+}
